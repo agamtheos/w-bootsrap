14c5ecc92dc8a3b88d9bc413d6486ae67c155244
Agam Theos <76621666+agamtheos@users.noreply.github.com>
2021-03-26 20:03:55 +0700

    Add files via upload

---
+++ templates/footer.php
@@ -0,0 +1,6 @@
+
+ 
+    <footer class="section">
+        <div class="center grey-text">Copyright © 2021 Agam Theos</div>
+    </footer>
+</body>
---
+++ templates/header.php
@@ -0,0 +1,27 @@
+<head>
+  <title>Agam Theos</title>
+  <!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <style type="text/css">
+     .brand{
+         background: #cbb09c !important;
+     }
+     .brand-text{
+         color: #cbb09c !important;
+     }
+     form{
+         max-width: 460px;
+         margin: 20px auto;
+         padding: 20px;
+     }
+  </style>
+</head>
+  <body class="grey lighten-4">
+    <nav class="white z-depth-0">
+        <div class="container">
+            <a href="index.php" class="brand-logo brand-text">Data Perusahaan</a>
+            <ul id="navmobile" class="right hide-on-small-and-down">
+                <li><a href="add_perusahaan.php" class="btn brand z-depth-0">Isi Data</a></li>
+            </ul>
+        </div>
+    </nav>
