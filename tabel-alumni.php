5f43c6701d92374c30bd409d11385b80c2200e3c
agamtheos <agamtheos@gmail.com>
2021-03-29 06:04:27 +0700

    tambah tabel-alumin.php

---
+++ tabel-alumni.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Data Alumni</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+  <script src="main.js"></script>
+  <title>Document</title>
+</head>
+<body>
+
+    <table>
+        <tr>
+            <th>Nama</th>
+            <th>Jenis Kelamin</th>
+            <th>Email</th>
+            <th>Alamat</th>
+            <th>Email</th>
+            <th>Warga Negara</th>
+            <th>Status Perkawinan</th>
+            <th>Status Mahasiswa</th>
+        
+        </tr>
+    
+    </table>
+
+
+</body>
+</html>
